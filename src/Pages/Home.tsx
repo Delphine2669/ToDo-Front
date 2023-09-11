76af2e37b447c64e727c7e8b8a1933cf5412eed3
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-09-11 20:55:40 +0200

    changing layout and opengraph

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,6 +2,8 @@ import "../App.css";
 import Footer from "../components/Footer/Footer";
 import { Link } from "react-router-dom";
 import Header from "../components/Header/Header";
+import MTodoList from "../components/ToDo/MToDoList";
+import TTodoList from "../components/ToDo/TToDoList";
 function Home() {
   return (
     <>
@@ -14,6 +16,10 @@ function Home() {
           <Link to="/tv-shows" className="tv-show-link">
             tv-shows
           </Link>
+        </div>
+        <div className="to-do-display">
+          <MTodoList />
+          <TTodoList />
         </div>
         <Footer />
       </div>
